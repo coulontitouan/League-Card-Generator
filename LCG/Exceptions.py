61a7efe464ac71ce4ef8c57fa6c4047b5ffe1187
Livreur <coulontitouann@gmail.com>
2024-01-07 00:52:30 +0100

    Rename Exceptions.py to exceptions.py

--- LCG/Exceptions.py
+++
@@ -1,3 +0,0 @@
-class TitrePasDansBD(Exception):
-    def __init__(self):
-        self.message = "L'ID du titre n'est pas dans la BD à l'URL : 'https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/fr_fr/v1/challenges.json'"
